d77443a4943f47f1c40cb0523257b9a17579c8dd
Yuichi Nakamura <y.512.nakamura@gmail.com>
2022-12-22 23:18:57 +0900

    Fix write()

--- src/libx68k/write.c
+++ src/libx68k/write.c
@@ -11,13 +11,37 @@ ssize_t write(int fd, const void *buf, size_t count)
 {
   int res;
 
-  __asm__ volatile ("movel %1, -(%%sp)\n"
-                    "movel %2, -(%%sp)\n"
-                    "movew %3, -(%%sp)\n"
-                    ".hword 0xff40\n"
-                    "lea 10(%%sp),%%sp\n"
-                    "movel %%d0, %0"
-                    : "=d"(res) : "d"(count), "a"(buf), "d"(fd) : "%%d0");
+  if (isatty(fd)) {
+    char ch;
+
+    res = 0;
+    while (count-- > 0) {
+      ch = *(char *)buf++;
+      if (ch == '\n') {
+        __asm__ volatile ("movew %1, -(%%sp)\n"
+                          "movew %2, -(%%sp)\n"
+                          ".hword 0xff1d\n"
+                          "addql #4, %%sp\n"
+                          "movel %%d0, %0"
+                          : "=d"(res) : "d"(fd), "d"('\r') : "%%d0");
+      }
+      __asm__ volatile ("movew %1, -(%%sp)\n"
+                        "movew %2, -(%%sp)\n"
+                        ".hword 0xff1d\n"
+                        "addql #4, %%sp\n"
+                        "movel %%d0, %0"
+                        : "=d"(res) : "d"(fd), "d"(ch) : "%%d0");
+    }
+    res = count;
+  } else {
+    __asm__ volatile ("movel %1, -(%%sp)\n"
+                      "movel %2, -(%%sp)\n"
+                      "movew %3, -(%%sp)\n"
+                      ".hword 0xff40\n"
+                      "lea 10(%%sp),%%sp\n"
+                      "movel %%d0, %0"
+                      : "=d"(res) : "d"(count), "a"(buf), "d"(fd) : "%%d0");
+  }
 
   return res;
 }
