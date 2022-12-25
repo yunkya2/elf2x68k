b4c72aa198a3ca62d100ea3488bd7fc11706de53
Yuichi Nakamura <y.512.nakamura@gmail.com>
2022-12-25 22:43:06 +0900

    Update scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 M68K_TOOLCHAIN=${XDEV68K_DIR}/m68k-toolchain
 
 if [ ! -e ${M68K_TOOLCHAIN} ]; then
@@ -16,6 +18,11 @@ cp src/elf2x68k.py ${M68K_TOOLCHAIN}/bin
 cp src/x68k.ld ${M68K_TOOLCHAIN}/m68k-elf/lib
 cp src/x68k.specs ${M68K_TOOLCHAIN}/m68k-elf/lib
 
-(cd src/libx68k;make) && cp src/libx68k/libx68k.a ${M68K_TOOLCHAIN}/m68k-elf/lib
+(cd src/libx68k;make)
+cp src/libx68k/libx68k.a ${M68K_TOOLCHAIN}/m68k-elf/lib
+cp src/libx68k/libiocs/libiocs.a ${M68K_TOOLCHAIN}/m68k-elf/lib
+cp src/libx68k/libdos/libdos.a ${M68K_TOOLCHAIN}/m68k-elf/lib
+mkdir -p ${M68K_TOOLCHAIN}/m68k-elf/include/x68k
+cp -r src/libx68k/x68k ${M68K_TOOLCHAIN}/m68k-elf/include
 
 echo "Installed elf2x68k script into m68k-elf-gcc toolchain."
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 M68K_TOOLCHAIN=${XDEV68K_DIR}/m68k-toolchain
 
 if [ ! -e ${M68K_TOOLCHAIN} ]; then
@@ -16,5 +18,8 @@ rm -f ${M68K_TOOLCHAIN}/bin/elf2x68k.py
 rm -f ${M68K_TOOLCHAIN}/m68k-elf/lib/x68k.ld
 rm -f ${M68K_TOOLCHAIN}/m68k-elf/lib/x68k.specs
 rm -f ${M68K_TOOLCHAIN}/m68k-elf/lib/libx68k.a
+rm -f ${M68K_TOOLCHAIN}/m68k-elf/lib/libiocs.a
+rm -f ${M68K_TOOLCHAIN}/m68k-elf/lib/libdos.a
+rm -rf ${M68K_TOOLCHAIN}/m68k-elf/include/x68k
 
-echo "Uninstalled elf2x68k script into m68k-elf-gcc toolchain."
+echo "Uninstalled elf2x68k script from m68k-elf-gcc toolchain."
