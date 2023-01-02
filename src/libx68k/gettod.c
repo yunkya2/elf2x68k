a49f4623e2a26a0157c24cba04a92022a2d32ea6
Yuichi Nakamura <y.512.nakamura@gmail.com>
2023-01-03 00:28:37 +0900

    Implement gettimeofday()

--- src/libx68k/gettod.c
+++ src/libx68k/gettod.c
@@ -1,25 +1,39 @@
 /*
- * Stub version of gettimeofday.
+ * gettimeofday()
  */
 
 #include "config.h"
-#include <_ansi.h>
-#include <_syslist.h>
+#include <x68k/dos.h>
+#include <sys/types.h>
 #include <sys/time.h>
-#include <sys/times.h>
+#include <time.h>
 #include <errno.h>
 #undef errno
 extern int errno;
-#include "warning.h"
 
-struct timeval;
-
-int
-_gettimeofday (struct timeval  *ptimeval,
-        void *ptimezone)
+int gettimeofday(struct timeval *tv, void *tz)
 {
-  errno = ENOSYS;
-  return -1;
-}
+  uint32_t curdate;
+  uint32_t curtime;
+  struct tm tm;
+  int tm_mon;
 
-stub_warning(_gettimeofday)
+  if (tv != NULL) {
+    curdate = _dos_getdate();
+    curtime = _dos_gettim2();
+
+    tm.tm_sec  =  (curtime & 0x00003f);
+    tm.tm_min  =  (curtime & 0x003f00) >> 8;
+    tm.tm_hour =  (curtime & 0x1f0000) >> 16;
+    tm.tm_mday =  (curdate & 0x001f);
+    tm_mon     =  (curdate & 0x01e0) >> 5;
+    tm.tm_mon  =  tm_mon > 0 ? tm_mon - 1 : tm_mon;
+    tm.tm_year = ((curdate & 0xfe00) >> 9) + 80;
+    tm.tm_isdst = 0;
+
+    tv->tv_sec = mktime(&tm);
+    tv->tv_usec = 0;
+  }
+
+  return 0;
+}
