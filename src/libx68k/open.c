8ef3c2a78b312952f8c2bd4e6926049c1395f42f
Yuichi Nakamura <y.512.nakamura@gmail.com>
2023-06-14 00:00:28 +0900

    O_EXCL,O_APPEND,O_TRUNC support for open()

--- src/libx68k/open.c
+++ src/libx68k/open.c
@@ -13,22 +13,35 @@ int open(const char *file, int flags, ...)
 {
   int fd;
 
-  if (flags & (O_CREAT | O_EXCL)) {
-    fd = _dos_create(file, 0x20);   /* create normal file */
+  fd = _dos_open(file, flags & O_ACCMODE);
+
+  if (fd < 0) {
+    if (flags & O_CREAT) {
+      fd = _dos_create(file, 0x20);   /* create normal file */
+    }
+    if (fd < 0) {
+      errno = __doserr2errno(-fd);
+      return -1;
+    }
   } else {
-    fd = _dos_open(file, flags & O_ACCMODE);
+    if (flags & O_EXCL) {
+      _dos_close(fd);
+      errno = EEXIST;       /* file exist error */
+      return -1;
+    }
   }
 
-  if ((fd >= 0) && (__fd_assign (fd, file, flags) < 0))
-  {
+  if (flags & O_TRUNC) {
+    _dos_write(fd, 0, 0);   /* truncate exiting file */
+  }
+  if (flags & O_APPEND) {
+    _dos_seek(fd, 0, 2);    /* seek to end of the file */
+  }
+
+  if (__fd_assign (fd, file, flags) < 0) {
     _dos_close(fd);
     errno = EINVAL;
     return -1;
-  }
-
-  if (fd < 0) {
-    errno = __doserr2errno(-fd);
-    fd = -1;
   }
 
   return fd;
