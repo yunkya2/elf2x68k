42d893feabee8383cb4a63414251a54c4d9ed407
Yuichi Nakamura <y.512.nakamura@gmail.com>
2026-08-27 19:39:40 +0900

    libx68k: Enforce the maximum heap size in sbrk

--- src/libx68k/sbrk.c
+++ src/libx68k/sbrk.c
@@ -10,7 +10,7 @@ int __doserr2errno(int error);
 
 void *sbrk(ptrdiff_t incr)
 {
-  extern char *_HSTA, *_HEND;     /* Set by linker.  */
+  extern char *_HSTA, *_HEND, *_HMAX;     /* Set by linker.  */
   static char *heap_end;
   char *new_heap_end;
   char *prev_heap_end;
@@ -20,7 +20,7 @@ void *sbrk(ptrdiff_t incr)
       extern char _end;
       extern int _heap_size;
       _HSTA = &_end;
-      _HEND = _HSTA + _heap_size;
+      _HEND = _HMAX = _HSTA + _heap_size;
     }
     heap_end = _HSTA;
   }
@@ -31,12 +31,11 @@ void *sbrk(ptrdiff_t incr)
   if (new_heap_end > _HEND) {
     char *new_block_end;
     extern char *_PSP;
-    int res;
 
     /* Extend the memory block for heap */
 
     new_block_end = (char *)(((uint32_t)new_heap_end + 0x3fff) & ~0x3fff);
-    if (_PSP == 0 ||
+    if (_PSP == 0 || new_block_end > _HMAX ||
         _dos_setblock(_PSP, (uint32_t)new_block_end - (uint32_t)_PSP) < 0) {
       errno = ENOMEM;
       return (void *)-1;
